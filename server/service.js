828e0a8b8908b2783a682bd31f740796aebeacd5
HenriqueDev025 <carlossanttos365@gmail.com>
2025-10-31 21:44:01 -0300

    Inicio código 2
    Aula-01

---
+++ server/service.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.post('/api/login', (req, res) => {
+const { usuario, senha } = req.body;
+if 
+(usuario === 'admin' && senha === '1234')
+    { res.json({ status:'sucesso', mensagem: 'Login realizado com sucesso!' });}
+else 
+    { res.json({ status: 'erro', mensagem: 'Usuário ou senha inválidos.' });}
+});
+app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
---
+++ src/conponent/Form.jsx
@@ -0,0 +1,29 @@
+// LoginForm.jsx
+import React, { useState } from'react';
+function LoginForm() {
+const [usuario, setUsuario] = useState('');
+const [senha, setSenha] = useState('');
+const [mensagem, setMensagem] = useState('');
+const handleSubmit = async (e) => {
+e.preventDefault();
+const resposta = await fetch('http://localhost:3000/api/login',{
+method:'POST',
+headers: { 'Content-Type':'application/json' },
+body: JSON.stringify({ usuario, senha })
+});
+const dados = await resposta.json();
+setMensagem(dados.mensagem);
+};
+return (
+    <div>
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Usuário" value={usuario} onChange={(e) => setUsuario(e.target.value)}/>
+            <input type="password" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
+            <button type="" submit>Entrar</button>
+        </form>
+        <p>{mensagem}</p>
+    </div>
+);
+}
+export default LoginForm
